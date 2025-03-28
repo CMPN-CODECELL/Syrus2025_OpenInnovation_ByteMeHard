887f03bc7a3a1c1652ea6258a8485a35d8e01f6c
Anushka3204 <145286475+Anushka3204@users.noreply.github.com>
2025-03-28 21:38:43 +0530

    chatbot and retailer side
    chatbot and retailer side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Retailer from "./pages/Retailer";
 import Login from "./pages/Login";
 import Payment from "./pages/Payment";
 import UserDashboard from "./pages/UserDashboard";
+import List_products from "./pages/List_products";
+import DashboardRetailer from "./pages/Retailer_side/DashboardRetailer";
+import Negotiate from "./pages/Retailer_side/Negotiate";
 
 export default function App() {
   return (
@@ -15,6 +18,10 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/producer" element={<Producer />} />
         <Route path="/retailer" element={<Retailer />} />
+        <Route path="/list_products" element={<List_products />} />
+        <Route path="/dashboard_retailer" element={<DashboardRetailer />} />
+        <Route path="/negotiate" element={<Negotiate />} />
+
 
       </Routes>
     </Router>
