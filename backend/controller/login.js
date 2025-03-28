0de99b62437d1929903692ddb244c48266f6681e
Pranavlovescode <pranavtitambe04@gmail.com>
2025-03-28 12:44:31 +0530

    completed the login

--- backend/controller/login.js
+++ backend/controller/login.js
@@ -1,42 +1,96 @@
 const jwt = require("jsonwebtoken");
 const Manufacturer = require("../models/manufacturer");
+const Retailer = require("../models/retailer");
+const Supplier = require("../models/raw_material_seller");
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  if (!email || !password) {
-    return res.status(400).send("Please provide all the required fields");
-  }
-  const user = await Manufacturer.find({ email });
-  if (!user.length) {
-    return res.status(404).send("User not found");
-  }
-  console.log(user);
-  try {
-    if (user[0].password !== password) {
-      return res.status(401).send("Invalid credentials");
+    const { email, password } = req.body;
+    console.log(email, password);
+    if (!email || !password) {
+        return res.status(400).send("Please provide all the required fields");
     }
+    const manufacturer = await Manufacturer.find({ email });
+    const retailer = await Retailer.find({ email });
+    const supplier = await Supplier.find({ email });
+    console.log(manufacturer, retailer, supplier);
+    if (!manufacturer.length && !retailer.length && !supplier.length) {
+        return res.status(404).send("User not found");
+    }
+    console.log(user);
+    try {
+        if (email == manufacturer[0].email) {
 
-    const data = {
-      userId: user[0]._id,
-    };
-    console.log(String(user[0]._id), data);
-    const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1d" });
-    console.log(token);
-    res.cookie("UserToken", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "strict",
-      maxAge: 1000 * 60 * 60 * 24,
-    });
+            if (user[0].password !== password) {
+                return res.status(401).send("Invalid credentials");
+            }
 
-    return res.status(200).json({
-      username: user[0].email,
-      message: "User logged in successfully",
-    });
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
+            const data = {
+                userId: user[0]._id,
+            };
+            console.log(String(user[0]._id), data);
+            const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1d" });
+            console.log(token);
+            res.cookie("UserToken", token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24,
+            });
+
+            return res.status(200).json({
+                username: user[0].email,
+                message: "User logged in successfully",
+            });
+        }
+        else if(email == retailer[0].email){
+            if (user[0].password !== password) {
+                return res.status(401).send("Invalid credentials");
+            }
+    
+            const data = {
+                userId: user[0]._id,
+            };
+            console.log(String(user[0]._id), data);
+            const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1d" });
+            console.log(token);
+            res.cookie("UserToken", token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24,
+            });
+    
+            return res.status(200).json({
+                username: user[0].email,
+                message: "User logged in successfully",
+            });
+        }
+        else{
+            if (user[0].password !== password) {
+                return res.status(401).send("Invalid credentials");
+            }
+    
+            const data = {
+                userId: user[0]._id,
+            };
+            console.log(String(user[0]._id), data);
+            const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1d" });
+            console.log(token);
+            res.cookie("UserToken", token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24,
+            });
+    
+            return res.status(200).json({
+                username: user[0].email,
+                message: "User logged in successfully",
+            });
+        }
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 };
 
 module.exports = { login };
